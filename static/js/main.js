b3e9824b3ed2e089260986a0ac70fb6445c8cf25
Kartikey Mishra <76617485+KartikeyMish@users.noreply.github.com>
2023-05-15 14:39:35 +0530

    classification page changes

--- static/js/main.js
+++ static/js/main.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('#result').hide();
 
     // Upload Preview
-    function readURL(input) {
+    function readURL(input) {   
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
@@ -16,7 +16,7 @@ $(document).ready(function () {
             reader.readAsDataURL(input.files[0]);
         }
     }
-    $("#imageUpload").change(function () {
+    $("#file").change(function () {
         $('.image-section').show();
         $('#btn-predict').show();
         $('#result').text('');
@@ -60,5 +60,22 @@ $(document).ready(function () {
             $('.navbar').removeClass('solid');
         }
       });
+      (function() {
+  
+        'use strict';
+      
+        $('.input-file').each(function() {
+          var $input = $(this),
+              $label = $input.next('.js-labelFile'),
+              labelVal = $label.html();
+          
+         $input.on('change', function(element) {
+            var fileName = '';
+            if (element.target.value) fileName = element.target.value.split('\\').pop();
+            fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass('has-file').html(labelVal);
+         });
+        });
+      
+      })();
 
 });
